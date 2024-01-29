030f73956a364e75b761be68527cb804cb5bd15c
Udhay Manhas <udhay.manhas@gmail.com>
2024-01-29 20:43:53 +0530

    basic app setup, assets, layout, home, css

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover & Share</h1>
+    </section>
+  );
+};
+export default Home;
