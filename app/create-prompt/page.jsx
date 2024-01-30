5ce12e8d8f1a9e7ddf8c8a9ed7a89780e8917b8b
Udhay Manhas <udhay.manhas@gmail.com>
2024-01-30 16:37:53 +0530

    read prompts in feed

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -24,7 +24,7 @@ const CreatePrompt = () => {
         method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
-          useId: session?.user.id,
+          userId: session?.user.id,
           tag: post.tag,
         }),
       });
